ca321d9f7ae4ad69457c2ed2356bcda31a49f421
Aravind SV <arvind.sv@gmail.com>
2014-09-19 14:51:46 -0400

    Style changes for hound.

--- spec/models/oauth2_provider/token_spec.rb
+++ spec/models/oauth2_provider/token_spec.rb
@@ -1,4 +1,5 @@
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
+require File.expand_path(File.join(
+  File.dirname(__FILE__), "..", "..", "spec_helper"))
 
 module Oauth2Provider
   describe Oauth2Provider::Token do
--- spec/views/oauth2_provider/user_tokens/index_html_spec.rb
+++ spec/views/oauth2_provider/user_tokens/index_html_spec.rb
@@ -1,9 +1,10 @@
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "..", "spec_helper"))
+require File.expand_path(File.join(
+  File.dirname(__FILE__), "..", "..", "..", "spec_helper"))
 
 module Oauth2Provider
-  describe 'oauth2_provider/user_tokens/index.html.erb' do
+  describe "oauth2_provider/user_tokens/index.html.erb" do
     before :each do
-      @oauth_engine = double('oauth engine')
+      @oauth_engine = double("oauth engine")
       allow(view).to receive(:oauth_engine).and_return(@oauth_engine)
     end
 
@@ -12,7 +13,8 @@ module Oauth2Provider
 
       render
 
-      expect(response).to have_selector(".content_wrapper_inner .info-box", :text => "There are no tokens.")
+      expect(response).to have_selector(".content_wrapper_inner .info-box",
+                                        text: "There are no tokens.")
     end
 
     it "should display tokens when present" do
@@ -21,32 +23,35 @@ module Oauth2Provider
       @tokens = [token_1, token_2]
 
       expect(view).to receive(:url_for).
-        with({
-          :action => :revoke,
-          :token_id => token_1.id,
-          :controller => 'user_tokens'}).
+        with(action: :revoke,
+             token_id: token_1.id,
+             controller: "user_tokens").
         and_return("/revoke/token_1")
 
       expect(view).to receive(:url_for).
-        with({
-          :action => :revoke,
-          :token_id => token_2.id,
-          :controller => 'user_tokens'}).
+        with(action: :revoke,
+             token_id: token_2.id,
+             controller: "user_tokens").
         and_return("/revoke/token_2")
 
       render
 
-      Capybara.string(response.body).all("table#oauth_user_token_table tr").tap do |rows|
-        rows[1].all("td").tap do |columns|
-          expect(columns[0].text).to eq(token_1.oauth_client.name)
-          expect(columns[1]).to have_selector("a.delete_link[@href='/revoke/token_1']", :text => "Delete")
-        end
+      Capybara.string(response.body).all("table#oauth_user_token_table tr").
+        tap do |rows|
+          rows[1].all("td").tap do |columns|
+            expect(columns[0].text).to eq(token_1.oauth_client.name)
+            expect(columns[1]).
+              to have_selector("a.delete_link[@href='/revoke/token_1']",
+                               text: "Delete")
+          end
 
-        rows[2].all("td").tap do |columns|
-          expect(columns[0].text).to eq(token_2.oauth_client.name)
-          expect(columns[1]).to have_selector("a.delete_link[@href='/revoke/token_2']", :text => "Delete")
+          rows[2].all("td").tap do |columns|
+            expect(columns[0].text).to eq(token_2.oauth_client.name)
+            expect(columns[1]).
+              to have_selector("a.delete_link[@href='/revoke/token_2']",
+                               text: "Delete")
+          end
         end
-      end
     end
   end
 end
